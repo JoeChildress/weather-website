86b8b0b00bd7b4afdf9a3a08fb15997086b54e24
Joseph Childress <josephchildress@Josephs-MacBook-Pro.local>
2020-11-10 18:03:25 -0800

    updated fetch to use app api

--- public/js/app.js
+++ public/js/app.js
@@ -12,23 +12,21 @@ weatherForm.addEventListener('submit', (e) => {
 
     if (location) {
         msg1.textContent = `Loading weather information for ${location}...`
-        fetch(`http://api.weatherstack.com/current?access_key=7a9429cce317426c7f66e90440902dfc&query=${location}&units=f`).then((response) => {
+        msg2.textContent = "";
+
+        fetch(`/weather?address=${location}`).then((response) => {
+            
             response.json().then((data) => {
                 if (data.error) {
-                    msg1.textContent = "Error: " + data.error.code + " " + data.error.info;
+                    msg1.textContent = "Error: " + data.error;
                     msg2.textContent = ""
                     console.log("Error: ", data.error);
                 } else {
-                    const weatherIcon = document.createElement('img');
-                    weatherIcon.src = data.current.weather_icons[0];
-                    msg1.textContent = location + " ";
-                    msg1.appendChild(weatherIcon);
-                    msg2.textContent = `${data.request.query}: ${data.current.weather_descriptions[0]}, ${data.current.temperature}f`;
+                    msg1.textContent = data.location;
+                    msg2.textContent = data.forecast;
                 }
             })
         });
-
-
     } else {
         msg1.textContent = "Error: Please enter a location."
     }
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express();
 const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
+const port = process.env.PORT || 3000;
 
 //define paths for express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -93,6 +94,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => { //port
-    console.log("Server is running on port 3000")
+app.listen(port, () => { //port
+    console.log(`Server is running on port ${port}`);
 }); 
