91ab351143e0606773a3a5f9d5cc7d25d34b4c62
Joseph Childress <josephchildress@Josephs-MacBook-Pro.local>
2020-11-10 18:32:41 -0800

    updated the data and body text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,9 +8,8 @@ const forecast = (lat,long,callback) => {
         } else if (body.error) {
             callback("Unable to find location");
         } else {
-
             const current = body.current;
-            const info = `${body.location.name}: ${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. There is a ${current.precip} chance of rain.`
+            const info = `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. There is a ${current.precip} chance of rain.`
             callback(undefined,info);
         } 
     })
